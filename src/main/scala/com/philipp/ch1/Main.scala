2276e482b66d43a922410536435ea8fa17ff8b42
Philipp B. Costa <philcoder@gmail.com>
2019-10-22 15:59:47 -0300

    porting hacker rank problem evaluating e^x from erlang to scala

---
+++ src/main/scala/com/philipp/ch1/Main.scala
@@ -0,0 +1,27 @@
+package com.philipp.ch1
+
+import scala.annotation.tailrec
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    example1()
+  }
+
+  //Unit is like 'void' on java
+  def example1(): Unit ={
+    var capital = Map("US" -> "Washington", "France" -> "Paris")
+    capital += ("Japan" -> "Tokyo")
+    capital += ("Brazil" -> "BrasIlha")
+    println(capital)
+    println(capital("US"))
+
+    println("fatorial")
+    println(factorial(5))
+    println(factorial(20))
+
+    var sol = new Solution9()
+    sol.execute()
+  }
+
+  def factorial(x: BigInt): BigInt = if (x == 0) 1 else x * factorial(x - 1)
+}
---
+++ src/main/scala/com/philipp/ch1/Solution9.scala
@@ -0,0 +1,37 @@
+package com.philipp.ch1
+
+import scala.annotation.tailrec
+
+// Hacker-rank Problem: Evaluating e^x
+/*
+  Expected output:
+    2423600.1887
+    143.6895
+    1.6487
+    0.6065
+ */
+class Solution9 {
+  def execute(): Unit ={
+    var evaluate = List(20.0000, 5.0000, 0.5000, -0.5000)
+    for (elem <- evaluate) println("%.4f".format(calcule(elem)))
+  }
+
+  private def calcule(value: Double): Double ={
+    //only 9 rounds of calculation
+    @tailrec def evaluate(value: Double, count:Int, sum:Double): Double ={
+      if(count > 0) {
+        evaluate(value, count - 1, sum + Math.pow(value, count) / factorial(count))
+      }else{
+        1 + sum
+      }
+    }
+    evaluate(value, 9, 0)
+  }
+
+  private def factorial(n: BigInt): Double =
+  {
+    // Using tail recursion on scala need to use acc on recursion operations
+    @tailrec def factorialAcc(acc: BigInt, n: BigInt): BigInt = if (n <= 1) acc else factorialAcc(n * acc, n - 1)
+    factorialAcc(1, n).toDouble
+  }
+}
