99701a32858cbe450f3d9f4f1c1c6ca7664b0037
Philipp B. Costa <philcoder@gmail.com>
2019-10-25 16:50:07 -0300

    resolve HR Missing Number

---
+++ src/main/scala/com/philipp/hackerrank/MissingNumber.scala
@@ -0,0 +1,25 @@
+package com.philipp.hackerrank
+
+import scala.collection.mutable.ArrayBuffer
+import scala.io.StdIn
+
+object MissingNumber {
+  def main(args: Array[String]): Unit = {
+    val input = io.Source.stdin.getLines().toList
+    val setA = input(1).split(" ").map(elem => elem.toInt).sorted
+    val setB = input(3).split(" ").map(elem => elem.toInt).sorted
+
+    //this difference list isn't optimal execution because exceed time on test4
+//    for(elem <- setA){
+//      val index = java.util.Arrays.binarySearch(setB, elem)
+//      if(index > -1){
+//        //remove at index
+//        setB = setB.patch(index, Nil, 1)
+//      }
+//    }
+
+    //the native 'diff' is more fast execution
+    //distinct remove duplicated
+    setB.diff(setA).distinct.sorted.foreach(elem => print(s"$elem "))
+  }
+}
